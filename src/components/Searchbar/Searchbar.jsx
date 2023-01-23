cb9d7511f8a0f0f5ac27979e2d174a476b8dc194
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2023-01-23 20:40:25 +0100

    fixed

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -39,29 +39,3 @@ export function Searchbar({ onSubmit }) {
   );
 }
 
-// export const Searchbar = ({ onSubmit }) => {
-
-//   const handleSubmit = (values, { resetForm }) => {
-//     onSubmit(values);
-//     resetForm();
-//   };
-
-//   return (
-// //     <Formik initialValues={{ searchQuery: '' }} onSubmit={handleSubmit}>
-//       <Header>
-//         <SearchForm>
-//           <SearchFormButton type="submit">
-//             <Icon />
-//           </SearchFormButton>
-//           <SearchFormInput
-//             type="text"
-//             name="searchQuery"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//           />
-//         </SearchForm>
-//       </Header>
-//     </Formik>
-//   );
-// };
